e14da675b8f4a633b4602c8e08f10e8dec081b5b
DyerSituation <ead2174@columbia.edu>
2016-06-03 11:51:58 -0700

    single file OOP RPS game complete

--- RockPaperScissors.rb
+++ RockPaperScissors.rb
@@ -1,9 +1,3 @@
-def computer_turn
-  moves = ["r", "p", "s"]
-  chosen_move = moves[rand(0..2)]
-  return chosen_move
-end
-
 class Player
   def initialize(name = "Player")
     @name = name
@@ -24,69 +18,92 @@ class Player
   attr_reader :wins
   attr_reader :losses
   attr_reader :draws
-  def Player.who_wins(input_p, input_c, player, computer)
+end
+
+class Game
+  @player
+  @computer
+  @loop_i
+  @result
+  @answer
+  def init
+    puts "What is your name?"
+    name = gets.chomp
+
+    @player = Player.new(name)
+    @computer = Player.new("Computer")
+    @loop_i = 0
+  end
+
+  def play
+    loop do
+      @loop_i += 1
+      puts "round #{@loop_i} start"
+      sleep(1)
+      puts "rock (r), paper (p), or scissors (s)?"
+      player_move = gets.chomp
+      computer_move = computer_turn
+      sleep(1)
+      puts "\n#{@player.name} chose: #{player_move}"
+      sleep(1.5)
+      puts "\nComputer chose: #{computer_move}\n"
+      sleep(1)
+      who_wins(player_move, computer_move)
+
+      loop do
+        puts "\nPlay again? (y/n)\n"
+        @answer = gets.chomp
+        if !(@answer == "n" || @answer == "y")
+          puts "Invalid input"
+        else
+          break
+        end
+      end
+      if @answer == "n"
+        break
+      end
+    end
+    goodbye
+  end
+
+  def goodbye
+    puts "Thanks for playing, #{@player.name}"
+  end
+
+  def computer_turn
+    moves = ["r", "p", "s"]
+    moves.sample
+  end
+
+  def who_wins(input_p, input_c)
     if input_p == input_c
-      player.increment_draws
-      computer.increment_draws
-      result = "d"
+      @player.increment_draws
+      @computer.increment_draws
+      @result = "d"
     elsif (input_p == "r" && input_c == "s") || (input_p == "p" && input_c == "r") || (input_p == "s" && input_c == "p")
-      player.increment_wins
-      computer.increment_losses
-      result = "w"
+      @player.increment_wins
+      @computer.increment_losses
+      @result = "w"
     else
-      computer.increment_wins
-      player.increment_losses
-      result = "l"
+      @computer.increment_wins
+      @player.increment_losses
+      @result = "l"
     end
-    Player.print_winner(result, player, computer)
-
+    print_winner
   end
 
-  def Player.print_winner(result, player, computer)
-    if result == "d"
+  def print_winner()
+    if @result == "d"
       puts "It's a draw!"
-    elsif result == "w"
-      puts "#{player.name} wins!"
-    elsif result == "l"
-      puts "#{computer.name} wins!"
+    elsif @result == "w"
+      puts "#{@player.name} wins!"
+    elsif @result == "l"
+      puts "#{@computer.name} wins!"
     end
 
-    puts "Your stats: \nwins: #{player.wins} \nlosses: #{player.losses} \ndraws: #{player.draws}"
+    puts "Your stats: \nwins: #{@player.wins} \nlosses: #{@player.losses} \ndraws: #{@player.draws}"
   end
 end
-
-
-
-
-puts "What is your name?"
-name = gets.chomp
-
-player = Player.new(name)
-computer = Player.new("Computer")
-i = 0
-while 1 == 1
-  i += 1
-  puts "round #{i} start"
-  sleep(1)
-
-  puts "rock (r), paper (p), or scissors (s)?"
-  player_move = gets.chomp
-  computer_move = computer_turn
-  sleep(1)
-  puts "\n#{player.name} chose: #{player_move}"
-  sleep(2)
-  puts "\nComputer chose: #{computer_move}\n"
-  sleep(1)
-  Player.who_wins(player_move, computer_move, player, computer)
-
-  puts "\nPlay again? (y/n)\n"
-  if (gets.chomp) == "n"
-    break
-  end
-end
-
-puts "\nGame over!"
-sleep(1)
-puts "\nYour stats: \nwins: #{player.wins} \nlosses: #{player.losses} \ndraws: #{player.draws}"
-sleep(1)
-puts "\nThanks for playing, #{player.name}."
+game = Game.new
+game.init
+game.play

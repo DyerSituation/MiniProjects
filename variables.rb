c78a61c9e23fbbdd8984da8ff049bdc12fc06b5a
DyerSituation <ead2174@columbia.edu>
2016-06-14 12:09:47 -0700

    Restructure folders, added go

--- variables.rb
+++
@@ -1,7 +0,0 @@
-f = File.new("generated_emails.csv",  "w+")
-f.truncate 0
-
-f.puts "email,inbox,\n"
-(1..300).each do |i|
-  f.puts "workforce#{i}@e.rainforestqa.com,https://e.rainforestqa.com/workforce#{i},\n"
-end

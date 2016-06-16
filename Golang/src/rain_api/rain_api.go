136f750e9408fef6b1e7b11cd6c51cd24a8e62bc
DyerSituation <ead2174@columbia.edu>
2016-06-15 17:21:16 -0700

    refactored so does not need tokens

--- Golang/src/rain_api/rain_api.go
+++ Golang/src/rain_api/rain_api.go
@@ -3,15 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
-
-func readToken(dec *json.Decoder) (t json.Token) {
-	t, err := dec.Token()
-	panicError(err)
-	fmt.Printf("%v\n", t)
-	return
-}
 
 func panicError(err error) {
 	if err != nil {
@@ -20,24 +14,17 @@ func panicError(err error) {
 }
 
 func main() {
-	fmt.Printf("%v\n", "List of virtual machines")
-	var m interface{}
+	fmt.Println("List of virtual machines")
+	var m []string
+
 	r, err := http.Get("https://app.rainforestqa.com:443/api/1/vm_stack.json")
-	dec := json.NewDecoder(r.Body)
+	rawData, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(rawData, m)
+	for _, ip := range m {
+		fmt.Printf("\t%v\n", ip)
+	}
+
+	//dec := json.NewDecoder(r.Body)
 	defer r.Body.Close()
 	panicError(err)
-
-	//read open bracket
-	readToken(dec)
-
-	for dec.More() {
-		// decode an array value (Message)
-		dec.Decode(&m)
-		panicError(err)
-		fmt.Printf("	%v\n", m)
-	}
-	// read closing bracket
-	readToken(dec)
-
-	r.Body.Close()
 }
--- Golang/src/rain_api/rain_test.go
+++ Golang/src/rain_api/rain_test.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"testing"
 )
 
-func TestOutput(t *testing.T) {
-	t.Log("cool")
+func TestToken(t *testing.T) {
+
+	const jsonStream = `[`
+
+	r, err := http.Get("https://app.rainforestqa.com:443/api/1/vm_stack.json")
+	dec := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	panicError(err)
+	token := readToken(dec)
+	fmt.Printf("Token type: %T", jsonStream)
+	//ar expected = json.Delim(`[`)
+	if token != jsonStream {
+		t.Error(token)
+	}
 }
